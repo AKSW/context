bbbb57a8dadc580b8810285f2e2be721b4d9c1dc
Tim Ermilov <yamalight@gmail.com>
2014-03-04 15:06:24 +0100

    refactor dup code

--- controllers/auth/auth.linkedin.js
+++ controllers/auth/auth.linkedin.js
@@ -44,26 +44,13 @@ var registerNewUser = function(profile, done) {
         }]
     };
 
-    // check username
-    User.findOne({username: user.username}, function(err, exuser) {
+    // register
+    User.registerNewUser(user, function(err, userData) {
         if(err) {
             return done(err);
         }
 
-        // if username is taken, append random string
-        if(exuser) {
-            user.username += Date.now();
-        }
-
-        // save
-        var userData = new User(user);
-        userData.save(function(err) {
-            if(err) {
-                return done(err);
-            }
-
-            return done(null, userData);
-        });
+        return done(null, userData);
     });
 };
 
--- db/user.js
+++ db/user.js
@@ -17,6 +17,31 @@ userSchema = new Schema({
     }]
 });
 
+// custom registration method with additional checks
+userSchema.statics.registerNewUser = function (user, cb) {
+    // check username
+    this.findOne({username: user.username}, function(err, exuser) {
+        if(err) {
+            return cb(err, null);
+        }
+
+        // if username is taken, append random string
+        if(exuser) {
+            user.username += Date.now();
+        }
+
+        // save
+        var userData = new User(user);
+        userData.save(function(err) {
+            if(err) {
+                return cb(err, null);
+            }
+
+            return cb(null, userData);
+        });
+    });
+};
+
 // Model
 User = mongoose.model('users', userSchema);
 
