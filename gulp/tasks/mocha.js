ef1fba836ebbf38ba49122a49f3cf23121cb358f
Tim Ermilov <yamalight@gmail.com>
2014-04-11 19:27:37 +0200

    increase timeout for tests to let long suites succesfully pass

--- gulp/tasks/mocha.js
+++ gulp/tasks/mocha.js
@@ -2,5 +2,9 @@ var gulp = require('gulp');
 var mocha = require('gulp-mocha');
 
 module.exports = function(){
-    gulp.src('./tests/**/*.js').pipe(mocha({reporter: 'spec'}));
+    gulp.src('./tests/**/*.js')
+    .pipe(mocha({
+        reporter: 'spec',
+        timeout: 20000, // 10s
+    }));
 };
