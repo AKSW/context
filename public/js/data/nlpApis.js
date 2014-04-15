e4fb558d62bf8847e1cf9b326db313ff2e417b7a
Tim Ermilov <yamalight@gmail.com>
2014-04-15 12:48:13 +0200

    delete german spotlight from ui

--- public/js/data/nlpApis.js
+++ public/js/data/nlpApis.js
@@ -10,11 +10,6 @@ collection.push({
     id: 'FOX',
     name: 'FOX',
 });
-// german spotlight
-collection.push({
-    id: 'DBpedia-Spotlight-DE',
-    name: 'DBpedia Spotlight (German)',
-});
 
 // Our module now returns our view
 module.exports = collection;
