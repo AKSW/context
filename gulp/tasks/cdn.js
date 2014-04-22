18d72d752fb45d79d37385aaae7b2aa7084d1321
Tim Ermilov <yamalight@gmail.com>
2014-04-22 16:52:05 +0200

    cdnize all possible libs

--- gulp/tasks/cdn.js
+++ gulp/tasks/cdn.js
@@ -30,6 +30,18 @@ module.exports = {
                 {
                     file: '/bower_components/angular-strap/dist/angular-strap.tpl.min.js',
                     cdn: '//cdnjs.cloudflare.com/ajax/libs/angular-strap/2.0.0/angular-strap.tpl.min.js'
+                },
+                {
+                    file: '/bower_components/angular-ui-router/release/angular-ui-router.js',
+                    cdn: '//cdnjs.cloudflare.com/ajax/libs/angular-ui-router/0.2.8/angular-ui-router.min.js'
+                },
+                {
+                    file: '/bower_components/lodash/dist/lodash.compat.js',
+                    cdn: '//cdnjs.cloudflare.com/ajax/libs/lodash.js/2.4.1/lodash.min.js'
+                },
+                {
+                    file: '/bower_components/stringjs/lib/string.min.js',
+                    cdn: '//cdnjs.cloudflare.com/ajax/libs/string.js/1.8.0/string.min.js'
                 }]
             }))
             .pipe(rename('layout.cdn.dust'))
