6812f643eacc3ea048582350f5bb818ec2b7455e
Navid <nourbakhsh@cs.uni-bonn.de>
2014-06-20 19:27:05 +0200

    temp solution

---
+++ modules/inputProcessing/turtle.js
@@ -0,0 +1,173 @@
+// includes
+var ProgressReporter = require('../abstract/progressReporter');
+var util = require('util');
+// async stuff
+var async = require('asyncawait/async');
+var await = require('asyncawait/await');
+var async2 = require('async');
+// crypto
+var crypto = require('crypto');
+// promise
+var Promise = require('bluebird');
+// fs
+var fs = require('fs');
+// promisified readfile
+var readFile = Promise.promisify(fs.readFile);
+// logger
+var logger = require('../../logger');
+var rdfstore = require('rdfstore');
+var config = require('../../config');
+var courpusId;
+// process function
+var process = async(function(corpus) {
+
+    // generate unique url for piece
+    var urlBase = 'upload-file:///'+corpus._id.toString()+'/';
+    courpusId = corpus._id;
+    // init results
+    var results = [];
+
+    // get files
+    var files = corpus.files;
+    var file = files[0];
+    var i;
+    var turtleFile=await(readFile(file.path, 'utf8'));
+
+    var test = await( function() {
+        rdfstore.create({persistent: true,
+            engine: 'mongodb',
+            name: 'turtleimport', // quads in MongoDB will be stored in a DB named myappstore
+            overwrite: true,    // delete all the data already present in the MongoDB server
+            mongoDomain: 'localhost', // location of the MongoDB instance, localhost by default
+            mongoPort: 27017 // port where the MongoDB server is running, 27017 by default
+        }, function (store) {
+            store.load('text/turtle', turtleFile, function (success, results) {
+                if (success) {
+                    console.log("Import successfull. Triples importet: " + results);
+                    store.execute('PREFIX ns2:<http://www.w3.org/ns/dcat#> SELECT * WHERE{ ?s ?p ns2:Dataset }', function (success, results) {
+                        if (success) {
+
+                            return secondQuery(results);
+                        }
+                        else console.log("query1 result: "+ success );
+                    });
+
+
+                }
+                else console.log(success + results);
+            })
+        });
+    });
+    console.log("Test: " + test);
+    // process files
+    /*for(i = 0; i < files.length; i++) {
+        file = files[i];
+        var data = await(readFile(file.path, 'utf8'));
+
+        // hash input
+        var md5sum = crypto.createHash('md5');
+        md5sum.update(file.name);
+        // generate unique url for piece
+        var url = urlBase+md5sum.digest('hex')+'/'+Date.now();
+
+        // push to results
+        results.push({
+            corpuses: [corpus._id],
+            uri: url,
+            source: data,
+            title: file.name,
+        });
+    }*/
+
+    return results;
+});
+
+// module
+var FilesProcessing = function () {
+    ProgressReporter.call(this);
+
+    // name (also ID of processer used in client)
+    this.name = 'turtle';
+
+    // function
+    this.process = process;
+
+    return this;
+};
+
+function secondQuery(myArray){
+
+    rdfstore.create({persistent:true,
+            engine:'mongodb',
+            name:'turtleimport', // quads in MongoDB will be stored in a DB named myappstore
+            overwrite:false,    // delete all the data already present in the MongoDB server
+            mongoDomain:'localhost', // location of the MongoDB instance, localhost by default
+            mongoPort:27017 // port where the MongoDB server is running, 27017 by default
+        }, function(store){
+            var trippleIndex = 0;
+            var tripples = [];
+            var tripple;
+            var results = [];
+            async2.whilst(function () {
+                    tripples=[];
+                    return trippleIndex < myArray.length;
+                },
+                function (next) {
+                    store.execute('PREFIX ns2:<http://www.w3.org/ns/dcat#> SELECT * WHERE{ <'+myArray[trippleIndex].s.value+'> ?p ?o }', function(success,results) {
+                        if (success) {
+
+                            results.forEach(function (resulttripple) {
+                                tripple = store.rdf.createTriple(myArray[trippleIndex].s, resulttripple.p, resulttripple.o);
+                                tripples.push(tripple);
+
+                            });
+
+                            results.push(mapper(tripples));
+                            trippleIndex++;
+                            if (trippleIndex%500 ===0) console.log("Step: "+trippleIndex);
+                            if (trippleIndex === myArray.length) {
+                                console.log("done");
+                                return results; }
+                            next();
+                        }
+                    });
+                },
+                function (err) {
+                    // All things are done!
+                    if (err) console.log("Error! + " + err);
+                })
+
+
+        }
+    );
+
+}
+function mapper(tripples){
+    //console.log("Ich bin im Mapper!");
+    var doc = new Object();
+    doc.corpuses = [courpusId];
+    tripples.forEach(function(tripple){
+        if(tripple.predicate.value === "http://purl.org/dc/terms/description"){
+            doc.source =tripple.object.value;
+            doc.uri = tripple.subject.value;
+            if ((tripple.object.lang !== "") ||(tripple.object.lang !== "undefined")){
+                doc.language = tripple.object.lang;
+            }
+        }
+        else if(tripple.predicate.value === "http://purl.org/dc/terms/title"){
+            doc.title =tripple.object.value;
+        }
+        else{
+            //console.log(tripple);
+        }
+
+    });
+
+    //console.log(doc);
+    if ((doc.source==="undefiend") ||(doc.source === null)) doc.source = "";
+    return doc;
+}
+// Inherit from ProgressReporter
+util.inherits(FilesProcessing, ProgressReporter);
+
+module.exports = new FilesProcessing();
