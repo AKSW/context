45e5878811d65ad9dacfd74a8a415b86753368e4
Navid <nourbakhsh@cs.uni-bonn.de>
2014-06-27 17:23:14 +0200

    NLP Apis for  new languages added

---
+++ modules/annotationServices/dbpedia.fr.js
@@ -0,0 +1,103 @@
+// includes
+// async-await fetures
+var async = require('asyncawait/async');
+var await = require('asyncawait/await');
+// promise
+var Promise = require('bluebird');
+// promisified request
+var request = Promise.promisify(require('request'));
+// lodash
+var _ = require('lodash');
+// logger
+var logger = require('../../logger');
+
+// service url
+var SpotlightUrl = 'http://spotlight.sztaki.hu:2225/rest';
+
+// default headers
+var defaultHeaders = {'content-type': 'application/x-www-form-urlencoded', 'accept': 'application/json'};
+
+// default form data
+var defaultData = {
+    confidence: 0.20,
+    support: 20
+};
+
+// default options
+var defaultOptions = {
+    url: SpotlightUrl,
+    headers: defaultHeaders,
+    method: 'POST',
+};
+
+// process function
+var process = async(function(sourceText) {
+    // form data
+    var data = _.extend(defaultData, {text: sourceText});
+    // form request options
+    var options = _.extend(defaultOptions, {form: data});
+
+    // get data
+    var resp = await(request(options));
+    var body = resp[1];
+
+    // form response
+    var result = {
+        annotation: body,
+        entities: [],
+    };
+
+    // parse json
+    try {
+        body = JSON.parse(body);
+    } catch (e) {
+        logger.error('error parsing', body);
+    }
+
+    // process resources
+    if(body.Resources) {
+        body.Resources.forEach(function(resource) {
+            // create new entity
+            var entity = {
+                name: resource['@surfaceForm'],
+                uri: resource['@URI'],
+                offset: parseInt(resource['@offset'], 10),
+                precision: parseFloat(resource['@similarityScore']),
+            };
+
+            // get entity types
+            var types = [];
+            if(resource['@types'] && resource['@types'] !== '') {
+                // split coma separated types
+                var tmpTypes = resource['@types'].split(',');
+                // only pick types with DBpedia prefix
+                types = tmpTypes.filter(function(item) {
+                    var prefix = item.split(':')[0];
+                    return prefix === 'DBpedia';
+                });
+            } else {
+                // use misc by default
+                types.push('DBpedia:Misc');
+            }
+            // assing type to new entity
+            entity.types = types;
+            // append entity to result
+            result.entities.push(entity);
+        });
+    }
+
+    return result;
+});
+
+// module
+var DBPediaAnnotation = function () {
+    // name (also ID of processer used in client)
+    this.name = 'DBpedia-Spotlight-FR';
+
+    // function
+    this.process = process;
+
+    return this;
+};
+
+module.exports = new DBPediaAnnotation();
--- public/js/data/nlpApis.js
+++ public/js/data/nlpApis.js
@@ -15,6 +15,46 @@ collection.push({
     id: 'DBpedia-Spotlight-DE',
     name: 'DBpedia Spotlight (German)',
 });
+// Dutch spotlight
+collection.push({
+    id: 'DBpedia-Spotlight-NL',
+    name: 'DBpedia Spotlight (Dutch)',
+});
+// French spotlight
+collection.push({
+    id: 'DBpedia-Spotlight-FR',
+    name: 'DBpedia Spotlight (French)',
+});
+// Hungarian spotlight
+collection.push({
+    id: 'DBpedia-Spotlight-HU',
+    name: 'DBpedia Spotlight (Hungarian)',
+});
+// Italian spotlight
+collection.push({
+    id: 'DBpedia-Spotlight-IT',
+    name: 'DBpedia Spotlight (Italian)',
+});
+// Portuguese spotlight
+collection.push({
+    id: 'DBpedia-Spotlight-PT',
+    name: 'DBpedia Spotlight (Portuguese)',
+});
+// Russian spotlight
+collection.push({
+    id: 'DBpedia-Spotlight-RU',
+    name: 'DBpedia Spotlight (Russian)',
+});
+// Spanish spotlight
+collection.push({
+    id: 'DBpedia-Spotlight-ES',
+    name: 'DBpedia Spotlight (Spanish)',
+});
+// Turkish spotlight
+collection.push({
+    id: 'DBpedia-Spotlight-TR',
+    name: 'DBpedia Spotlight (Turkish)',
+});
 
 // Our module now returns our view
 module.exports = collection;
