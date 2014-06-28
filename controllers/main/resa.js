a4415c570f6e5bae61af3a2db745b9eaa570fcea
Ali Khalili <hyperir@gmail.com>
2014-06-28 12:02:45 +0200

    Added twitter API

--- controllers/main/resa.js
+++ controllers/main/resa.js
@@ -1,10 +1,27 @@
-// db
 var logger = require('../../logger');
+// config
+var config = require('../../config');
+var twitter = require('ntwitter');
+var twit = new twitter({
+  consumer_key: config.twitter.consumerKey,
+  consumer_secret: config.twitter.consumerSecret,
+  access_token_key: config.twitter.accessTokenKey,
+  access_token_secret: config.twitter.accessTokenSecret
+});
+
+//entity to search on twitter
+var search_word='Iran';
 
 module.exports = function(app) {
 
     // realtime analysis
     app.get('/resa', function(req, res) {
+      twit.stream('statuses/filter', { track: search_word}, function(stream) {
+        stream.on('data', function(tweet) {
+          logger.info(tweet.text);
+        });
+
+      });
         return res.render('resa');
     });
 };
