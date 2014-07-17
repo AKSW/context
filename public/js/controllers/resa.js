49807d6f555edfe66779233568e40c563b49d948
Ali Khalili <hyperir@gmail.com>
2014-07-17 14:31:21 +0200

    improve UI

--- public/js/controllers/resa.js
+++ public/js/controllers/resa.js
@@ -4,22 +4,26 @@ var defaultPort = '8081';
 //$element === angular.element() === jQuery() === $()
 //angular.element.find('.tweet');
 var watchList = [];
-module.exports = function ResaController($scope,$http,$sce) {
-    $scope.tweets=[];
-    $scope.tweets_number=0;
-    $scope.entities_number=0;
+module.exports = function ResaController($scope, $http, $sce) {
+    $scope.toggle_stop=0;
+    $scope.toggle_start=1;
+    $scope.tweets = [];
+    $scope.tweets_number = 0;
+    $scope.entities_number = 0;
     // analysis start function
     $scope.startAnalysis = function (e) {
         // prevent event
         e.preventDefault();
-        var keyword=$scope.keyword;
+        $scope.toggle_stop=1;
+        $scope.toggle_start=0;
+        var keyword = $scope.keyword;
         $http({
-            url: "/api/resa/start/"+keyword,
+            url: "/api/resa/start/" + keyword,
             method: "GET"
-        }).success(function(response){
+        }).success(function (response) {
             $scope.response = response;
             initStreamWebsocket($scope);
-        }).error(function(error){
+        }).error(function (error) {
             $scope.error = error;
         });
     }
@@ -27,12 +31,14 @@ module.exports = function ResaController($scope,$http,$sce) {
     $scope.stopAnalysis = function (e) {
         // prevent event
         e.preventDefault();
+        $scope.toggle_start=1;
+        $scope.toggle_stop=0;
         $http({
             url: "/api/resa/stop",
             method: "GET"
-        }).success(function(response){
+        }).success(function (response) {
             $scope.response = response;
-        }).error(function(error){
+        }).error(function (error) {
             $scope.error = error;
         });
     }
@@ -40,49 +46,63 @@ module.exports = function ResaController($scope,$http,$sce) {
         // prevent event
         e.preventDefault();
         watchList = [];
-        $scope.tweets=[];
-        $scope.tweets_number=0;
+        $scope.entities_number = 0;
+        $scope.tweets = [];
+        $scope.tweets_number = 0;
     }
-    var getEntityType=function(types){
-        if(!types.length){
+    var getEntityType = function (types) {
+        if (!types.length) {
             return 'Misc';
         }
-        var tmp,out='';
-        _.forEach(types, function(v) {
-            tmp=v.split(':');
-            if(tmp[1]=='Place' || tmp[1]=='Person' || tmp[1]=='Organisation'){
-                out= tmp[1];
+        var tmp, out = '';
+        _.forEach(types, function (v) {
+            tmp = v.split(':');
+            if (tmp[1] == 'Place' || tmp[1] == 'Person' || tmp[1] == 'Organisation') {
+                out = tmp[1];
             }
         })
-        if(out){
+        if (out) {
             return out;
-        }else{
+        } else {
             return 'Misc';
         }
 
     }
-    var prepareTweetText=function(text,entities){
-        _.forEach(entities, function(e) {
+    var convertToSlug = function (Text) {
+        return Text
+            .toLowerCase()
+            .replace(/[^\w ]+/g, '')
+            .replace(/ +/g, '-')
+            ;
+    }
+    var prepareTweetText = function (text, entities) {
+        _.forEach(entities, function (e) {
             text = text.replace(e.name, '&nbsp;<span resource="' + e.uri + '" class="r_entity r_' + getEntityType(e.types).toLowerCase() + '" typeOf="' + e.types + '">' + e.name + '</span>&nbsp;');
             updateWatchlist(e);
         });
         return text;
     }
-    var updateWatchlist=function(entity){
-        if(entity.name!= $scope.keyword){
-            if(watchList[entity.name]==undefined){
-                watchList[entity.name]={count:1, type:getEntityType(entity.types), uri:entity.uri};
+    var updateWatchlist = function (entity) {
+        if (entity.name != $scope.keyword) {
+            if (watchList[entity.name] == undefined) {
+                watchList[entity.name] = {count: 1, type: getEntityType(entity.types), uri: entity.uri};
                 $scope.entities_number++;
-            }else{
+            } else {
                 watchList[entity.name].count++;
             }
         }
     }
-    var initStreamWebsocket = function($scope) {
+    var updateD3Graph=function(){
+        //console.log(watchList);
+        for (var key in watchList) {
+
+        }
+    }
+    var initStreamWebsocket = function ($scope) {
         var location = document.location.hostname + ':' + defaultPort + document.location.pathname;
         console.log('connecting to socket', location);
         var socket = new WebSocket('ws://' + location);
-        socket.onerror = function(err) {
+        socket.onerror = function (err) {
             console.log(err);
         };
         socket.onopen = function () {
@@ -90,16 +110,18 @@ module.exports = function ResaController($scope,$http,$sce) {
         };
         socket.onmessage = function (event) {
             var data = JSON.parse(event.data);
-            $scope.$apply(function() {
+            $scope.$apply(function () {
                 //console.log(data);
-                data.text=prepareTweetText(data.text,data.entities);
+                data.text = prepareTweetText(data.text, data.entities);
                 $scope.tweets.unshift(data);
                 $scope.tweets_number++;
+                //update d3 graph
+                updateD3Graph();
                 //console.log(watchList);
             });
         };
         socket.onclose = function () {
-            $scope.$apply(function() {
+            $scope.$apply(function () {
 
             });
         };
