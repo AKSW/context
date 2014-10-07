79f265aea2004a8b0f6a13de5efe80b02772f562
Tim Ermilov <yamalight@gmail.com>
2014-10-07 13:20:15 +0200

    fix gulp init task

--- gulp/tasks/vendor.js
+++ gulp/tasks/vendor.js
@@ -1,19 +1,26 @@
-var gulp = require('gulp');
-var config = require('../../config');
-var mainBowerFiles = require('main-bower-files');
-var concat = require('gulp-concat');
-var gulpif = require('gulp-if');
-var uglify = require('gulp-uglify');
+var fs = require('fs');
+// only do work if config already exists
+if (fs.existsSync('../../config.js')) {
+    var gulp = require('gulp');
+    var config = require('../../config');
+    var mainBowerFiles = require('main-bower-files');
+    var concat = require('gulp-concat');
+    var gulpif = require('gulp-if');
+    var uglify = require('gulp-uglify');
 
-module.exports = function() {
-    return gulp.src(mainBowerFiles({
-        paths: {
-            bowerDirectory: './bower_components',
-            bowerJson: './bower.json'
-        },
-        filter: /\.js$/,
-    }))
-    .pipe(concat('vendor.min.js'))
-    .pipe(gulpif(!config.debug, uglify()))
-    .pipe(gulp.dest('./public/dist/'));
-};
+    module.exports = function() {
+        return gulp.src(mainBowerFiles({
+            paths: {
+                bowerDirectory: './bower_components',
+                bowerJson: './bower.json'
+            },
+            filter: /\.js$/,
+        }))
+        .pipe(concat('vendor.min.js'))
+        .pipe(gulpif(!config.debug, uglify()))
+        .pipe(gulp.dest('./public/dist/'));
+    };
+} else {
+    module.exports = function() {
+    };
+}
